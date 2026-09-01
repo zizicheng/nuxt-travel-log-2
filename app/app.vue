a6e05b72e79b4e210fd964ac967baba107a2f208
zizicheng <lttsd@foxmail.com>
2026-09-01 08:02:57 +0800

    :sparkles: setup tailwind / daisy ui / icon

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      Button <Icon name="i-tabler:sun" />
+    </button>
   </div>
 </template>
