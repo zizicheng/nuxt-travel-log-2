09d5ce0550f81651b22be67aa2d61a2759547026
zizicheng <lttsd@foxmail.com>
2026-09-02 08:24:13 +0800

    :sparkles: add landing page (#3)

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign In with github</a>
+    </div>
+  </div>
+</template>
