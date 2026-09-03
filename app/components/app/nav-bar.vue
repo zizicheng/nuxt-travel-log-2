19ef90de6ad28fba74285aecdf7bb0a5ebf4aab2
zizicheng <lttsd@foxmail.com>
2026-09-03 11:06:24 +0800

    :sparkles: add theme toggle

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="navbar bg-primary text-primary-content">
     <div class="navbar-start">
-      <NuxtLink to="/" class="btn btn-ghost text-xl">
+      <NuxtLink to="/" class="btn btn-ghost text-xl dark:text-white hover:text-black">
         Travel Log
       </NuxtLink>
     </div>
     <div class="navbar-end">
       <a class="btn btn-accent">Sign In with github</a>
+      <AppThemeToggle />
     </div>
   </div>
 </template>
